f930be3cd69026d45c19675060424f741af3131e
saeedm1988 <mohammadi.saeed1988@gmail.com>
2022-03-25 01:17:18 +0430

    contact,about, header and footer

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.func, name='url_index'),
+    path('', views.index, name='url_index'),
     path('<page>', views.pages, name='pages'),
+    path('posts/<slug:slug>', views.post_page, name='post_page'),
+    path('categories/<slug:cat_slug>', views.cat_page, name='cat_page'),
 ]
--- my_site/settings.py
+++ my_site/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'templates'
+            BASE_DIR / 'templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -119,7 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIFILES_DIRS = [
+
+STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
